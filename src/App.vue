7abad1c6292ca76e7f69190b76c985ee1ca76554
Sagynbek Esenbekov <jagger773@gmail.com>
2021-04-15 17:46:23 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container" @click="changeNameHandler">
+    {{name}} {{surname}}
+  </div>
+</template>
+<script>
+  export default{
+    props(){
+     
+    },
+    data(){
+      return {
+        name: "John",
+        surname: "kdsjfksdjf"
+      }
+    },
+    methods: {
+      changeNameHandler (){
+        if (this.surname === "Black") {
+           this.name = "Alice"
+        }
+      }
+    },
+    mounted() {
+      console.log("before if " + this.surname)
+       if(this.name === "John"){
+         this.surname= "Black";
+       }
+       console.log("after " + this.surname);
+    },
+    created() {
+
+    },
+    destroyed() {
+
+    }
+  }
+</script>
+<style >
+  .container {
+    color: red;
+    margin: 0 auto;
+    width: 80%;
+    font-size: 28px;
+  }
+</style>
